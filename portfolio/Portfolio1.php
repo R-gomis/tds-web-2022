3a523cfc64995dde158fa520a802d6a444e5f2cf
R-gomis <113612571+R-gomis@users.noreply.github.com>
2022-12-15 17:03:36 +0100

    Add files via upload

---
+++ portfolio/Portfolio1.php
@@ -0,0 +1,40 @@
+<?php
+$menu = array(
+  "accueil" => "Accueil",
+  "apropos"=> "Apropos",
+  "competences"=> "Compétences",
+  "experiences" => "Expériences",
+  "formation" => "Formation",
+  "contact" => "Contact"
+);
+
+$Apropos = array(
+   "Accroche" =>"",
+  "Présentation" =>"",
+  "Photo" =>"" 
+);
+  
+
+$Accueil = array( 
+  "Nom"=> "Gomis Richard",
+  "Sexe"=> "masculin",
+  "Date de naissance" => "01/06/1999 (23 ans)",
+  "Titre"=>"Futur Développeur Web"
+);
+ 
+$Compétences = array();
+  
+
+$Expériences = array(
+  "Uber eats" => "",
+    "Période" => "2018-2020",
+    "Lieu" => "Caen",
+  "Burger King"=> "",
+    "Période"=> "2019-2022",
+    "Lieu"=> "Caen",
+);
+  
+$Formation = array();
+
+$Contact = array();
+
